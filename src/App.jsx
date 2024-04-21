e8af39477a00f2580855cf645947bfe572f0c4b9
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-04-21 12:25:34 +0600

    express.js client side.

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {
+      name: name,
+      email: email
+    }
+    console.log(user)
+    fetch('http://localhost:5000/users', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+
+        form.reset();
+      })
+  }
+  return (
+    <>
+      <h1>User Management Client</h1>
+      <h3>Total user: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
+      {
+        users?.map(user => <p key={user.id}>{user.id} &nbsp; &nbsp; {user.name} &nbsp; &nbsp; {user.email}</p>)
+      }
+    </>
+  )
+}
+
+export default App
